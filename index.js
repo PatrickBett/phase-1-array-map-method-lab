c86a6dc441de3cb5af419b8c4d917a856b32c086
PatrickBett <patrickbett018@gmail.com>
2023-09-26 05:39:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const titleAsArray = tutorials.map(title => title.split(" "));
+  const capitalizeWords = titleAsArray.map(element => element.map(word => word.charAt(0).toUpperCase() + word.slice(1)));
+  const capitalizedTitles = capitalizeWords.map(element => element.join(" "));
+  return capitalizedTitles;
 }
